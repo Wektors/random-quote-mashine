bf1b3df3da0b58aaecb382da5b40c1b06dfc9d8b
Wektors <wiktorm16@gmail.com>
2022-10-15 21:30:55 +0200

    basic layout ready

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.sass";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+let quote = {};
+
+async function updateQuote(data) {
+	await fetch("https://api.quotable.io/random")
+		.then((response) => response.json())
+		.then((data) => console.log(data));
+}
+
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			content: quote.content,
+		};
+	}
+
+	componentDidMount() {
+		updateQuote();
+		this.setState(quote);
+		console.log(quote);
+	}
+	render() {
+		return (
+			<div id="root">
+				<div id="quote-box">
+					<div id="text">Quote Text{updateQuote}</div>
+					<div id="author">Quote author{this.state.content}</div>
+					<div id="quoteFooter">
+						<a
+							id="tweet-quote"
+							href="https://twitter.com/intent/tweet?text=Hello%20world"
+							target="_top"
+						>
+							<img
+								id="icon"
+								alt="twitter icon"
+								src="https://www.pinclipart.com/picdir/big/138-1388876_2017-intertech-americas-corp- white-twitter-bird-transparent.png"
+							/>
+						</a>
+						<div id="new-quote">
+							<button onClick={updateQuote}>NEW QUOTE</button>
+						</div>
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default App;
