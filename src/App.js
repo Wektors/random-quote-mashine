4b428383489f7fa952109d8d905bc30171b17084
Wektors <wiktorm16@gmail.com>
2022-10-15 22:48:10 +0200

    working

--- src/App.js
+++ src/App.js
@@ -1,33 +1,32 @@
 import React from "react";
 import "./App.sass";
 
-let quote = {};
-
-async function updateQuote(data) {
-	await fetch("https://api.quotable.io/random")
-		.then((response) => response.json())
-		.then((data) => console.log(data));
-}
-
 class App extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			content: quote.content,
+			content: "",
 		};
 	}
 
+	updateQuote = () => {
+		fetch("https://api.quotable.io/random")
+			.then((response) => response.json())
+			.then((data) => {
+				this.setState({ ...data });
+			});
+	};
+
 	componentDidMount() {
-		updateQuote();
-		this.setState(quote);
-		console.log(quote);
+		this.updateQuote();
 	}
+
 	render() {
 		return (
 			<div id="root">
 				<div id="quote-box">
-					<div id="text">Quote Text{updateQuote}</div>
-					<div id="author">Quote author{this.state.content}</div>
+					<div id="text">{this.state.content}</div>
+					<div id="author">{this.state.author}</div>
 					<div id="quoteFooter">
 						<a
 							id="tweet-quote"
@@ -41,7 +40,7 @@ class App extends React.Component {
 							/>
 						</a>
 						<div id="new-quote">
-							<button onClick={updateQuote}>NEW QUOTE</button>
+							<button onClick={this.updateQuote}>NEW QUOTE</button>
 						</div>
 					</div>
 				</div>
